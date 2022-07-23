449316f562ce9c231e4e382516efaf634d02a7b3
Nadia <enadialopez@gmail.com>
2022-07-23 19:39:50 -0300

    agrego la lista de categoria y estilo global

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Category } from './components/Category'
-
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './GobalStyles'
 
 const App = () => {
     return (
         <div>
-            <Category />
+            <GlobalStyle />
+            <ListOfCategories />
         </div>
     )
 }
