6f1170b14157608b4f355b0bffc3ea8dd77e2c29
Nadia <enadialopez@gmail.com>
2022-07-23 21:36:58 -0300

    agrego photocard, interactua con lista y agrego icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GobalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 const App = () => {
     return (
         <div>
             <GlobalStyle />
             <ListOfCategories />
+            <ListOfPhotoCards />
         </div>
     )
 }
 
-export default App;
+export default App
--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,13 +1,11 @@
-import React from "react";
-import { Anchor, Imagen } from "./styles";
-
+import React from 'react'
+import { Anchor, Imagen } from './styles'
 
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
 
-export const Category = ({cover =
-DEFAULT_IMAGE, path, emoji = '?'}) =>(
-    <Anchor href= {path}>
-        <Imagen src={cover} />
-        {emoji}
-    </Anchor>
+export const Category = ({cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
+  <Anchor href={path}>
+    <Imagen src={cover} />
+    {emoji}
+  </Anchor>
 )
